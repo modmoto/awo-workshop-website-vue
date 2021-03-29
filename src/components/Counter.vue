2bd186d8242f7a422e71612f014d8a428bf733fd
Simon Heiss <simonheiss87@gmail.com>
2021-03-29 11:07:08 +0200

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    Hallo Workshop, es wurde {{ counter }} mal gecklickt
+    <button @click="count">click</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Counter extends Vue {
+  public counter = 0;
+
+  public count(): void {
+    this.counter = this.counter + 1;
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
