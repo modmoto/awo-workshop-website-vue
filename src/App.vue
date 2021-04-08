ae9a61c0b0cd139abd450264383e88d1f8733f94
Simon Heiss <simonheiss87@gmail.com>
2021-04-08 17:21:24 +0200

    made super simple

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Counter />
+  <div>
+    Hallo zusammen
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import Counter from './components/Counter.vue';
 
-@Component({
-  components: {
-    Counter,
-  },
-})
-export default class App extends Vue {}
-</script>
+@Component({})
+export default class App extends Vue {
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-</style>
+</script>
