39f5c52146fb73b92f4a76bec11fa7b41d5605a5
Simon Heiss <simonheiss87@gmail.com>
2021-04-09 18:17:14 +0200

    minimal vue

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,12 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Vue } from 'vue-property-decorator';
 
-@Component({})
 export default class App extends Vue {
 
 }
+
 </script>
 
 <style>
