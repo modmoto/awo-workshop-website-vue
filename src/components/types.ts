89eb08b558c624785365fd979b711ec2ccbead26
Simon Heiss <simonheiss87@gmail.com>
2021-04-14 16:28:20 +0200

    class instead of interface

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,7 +1,15 @@
-export interface Greeting {
-    id: string,
-    greeting: string,
-    user: string,
-    image: string,
-    likes: number
+export class Greeting {
+    id;
+    greeting;
+    user;
+    image;
+    likes;
+
+    constructor(id: string, greeting: string, user: string, image: string, likes: number) {
+        this.id = id;
+        this.greeting = greeting;
+        this.user = user;
+        this.image = image;
+        this.likes = likes;
+    }
 }
